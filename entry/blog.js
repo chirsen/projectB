69e6b6b833dade9af6e8c8ee6985f0cb0d181cc9
吴奇生 <qisheng@eastmoney.com>
2016-10-26 16:28:35 +0800

    入口

--- entry/blog.js
+++ entry/blog.js
@@ -1,11 +1,13 @@
 require("../public/css/style.css");
 require("../public/css/theme.css");
 var Vue = require('vue');
-import ele_header from '../public/component/header/ele_header.vue'
-import ele_content from '../public/component/content/ele_content.vue'
-import ele_copyRight from '../public/component/copyRight/ele_copyRight.vue'
+var ele_header = require('../public/component/header/ele_header.vue');
+var ele_content = require('../public/component/content/ele_content.vue');
+var ele_copyRight = require('../public/component/copyRight/ele_copyRight.vue');
 
 var vue = new Vue({
     el: '#app',
-    components: { "ele-header": ele_header, "ele-content": ele_content, "ele-copyright": ele_copyRight }
+    components: { "ele-header": ele_header, "ele-content": ele_content, "ele-copyright": ele_copyRight },
+    create:function(){
+    }
 });
