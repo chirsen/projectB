9189eac1afe26e92cb3fa53965551e08972b9dae
吴奇生 <qisheng@eastmoney.com>
2016-11-01 16:54:26 +0800

    更改router指向

--- entry/blog.js
+++ entry/blog.js
@@ -5,6 +5,9 @@ var vueRouter = require('vue-router');
 
 Vue.use(vueRouter);
 
+if(module.hot)
+    module.hot.accept();
+
 var ele_header = require('../public/component/header/ele_header.vue');
 var ele_content = require('../public/component/content/ele_content.vue');
 var ele_copyRight = require('../public/component/copyRight/ele_copyRight.vue');
@@ -32,20 +35,22 @@ const router = new vueRouter({
                 "pages-list": pages_list,
             }
         }, {
-            path: '/:front/:type',
-            components: {
-                "location-list": location_list,
-                "content-list": content_list,
-                "rank-list": rank_list,
-                "date-list": date_list
-            }
-        }, {
             path: '/:front',
             components: {
                 "location-list": location_list,
                 "content-list": content_list,
                 "rank-list": rank_list,
-                "date-list": date_list
+                "date-list": date_list,
+                "pages-list": pages_list,
+            }
+        }, {
+            path: '/:front/:type',
+            components: {
+                "location-list": location_list,
+                "content-list": content_list,
+                "rank-list": rank_list,
+                "date-list": date_list,
+                "pages-list": pages_list,
             }
         }
     ]
@@ -56,7 +61,7 @@ new Vue({
     components: { "ele-header": ele_header, "ele-content": ele_content, "ele-copyright": ele_copyRight },
     created: function() {
         Bus.$on('rout', function(toUrl) {
-            router.push(toUrl);
+            router.push({path:toUrl});
         });
     }
 }).$mount('#app');
