3e44a8c339e6b9bcdd5d22c099dddda2407db2c7
吴奇生 <qisheng@eastmoney.com>
2016-11-01 16:55:52 +0800

    测试server

--- server.js
+++ server.js
@@ -3,6 +3,33 @@ var http = require('http'),
     fs = require('fs'),
     url = require('url');
 
+var list = [{
+                    title:"from Server",
+                    quto:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                    writeDate:"xxxx-xx-xx",
+                    scanNum:"10000",
+                    folder:"xx/xx"
+                },{
+                    title:"XXXX",
+                    quto:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                    writeDate:"xxxx-xx-xx",
+                    scanNum:"10000",
+                    folder:"xx/xx"
+                }];
+        var list2 = [{
+                    title:"from Server B",
+                    quto:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                    writeDate:"xxxx-xx-xx",
+                    scanNum:"10000",
+                    folder:"xx/xx"
+                },{
+                    title:"XXXX",
+                    quto:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                    writeDate:"xxxx-xx-xx",
+                    scanNum:"10000",
+                    folder:"xx/xx"
+                }];
+
 http.createServer(function(req, res) {
     var pathname = url.parse(req.url).pathname;
     var filepath = __dirname + pathname;
@@ -20,13 +47,18 @@ http.createServer(function(req, res) {
             case "home":
                 console.log(url.parse(req.url).query);
                 var callback = url.parse(req.url).query.split("&")[1].split("=")[0];
-                var data = callback+"("+{a:100}+")";
+                var data = callback+"("+JSON.stringify(list)+")";
             	res.writeHead(200, {"Content-Type": "text/javascript"});
                 res.end(data);
                 break;
             case "front":
                 break;
             case "node":
+                console.log(url.parse(req.url).query);
+                var callback = url.parse(req.url).query.split("&")[1].split("=")[0];
+                var data = callback+"("+JSON.stringify(list2)+")";
+                res.writeHead(200, {"Content-Type": "text/javascript"});
+                res.end(data);
                 break;
             case "multi":
                 break;
